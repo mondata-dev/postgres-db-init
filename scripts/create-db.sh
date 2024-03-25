a31c078a17b87c237c63cec57f714575302de33f
David Poetzsch-Heffter <david.poetzsch-heffter@mondata.de>
2024-03-25 17:25:29 +0100

    added privileges for schema public; required for anodetect db for some reason

--- scripts/create-db.sh
+++ scripts/create-db.sh
@@ -3,5 +3,6 @@
 psql -U ${PGADMINUSER} -h ${PGHOST} -d postgres -c "create user \"${PGUSER}\" with encrypted password '${PGUSERPASSWORD}'"
 psql -U ${PGADMINUSER} -h ${PGHOST} -d postgres -c "create database \"${PGDATABASE}\""
 psql -U ${PGADMINUSER} -h ${PGHOST} -d postgres -c "grant all privileges on database \"${PGDATABASE}\" to \"${PGUSER}\""
+psql -U ${PGADMINUSER} -h ${PGHOST} -d ${PGDATABASE} -c "grant all privileges on schema "public" to \"${PGUSER}\""
 
 exit 0
