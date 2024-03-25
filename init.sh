eee805f99cd02c54e3fe5a829e3fa180e572f0df
David Poetzsch-Heffter <david.poetzsch-heffter@mondata.de>
2024-03-25 17:35:33 +0100

    removed lookup as it caused problems and was not needed anyway

--- init.sh
+++ init.sh
@@ -4,11 +4,6 @@
 export PGUSERPASSWORD=${PGPASSWORD}
 export PGPASSWORD=${PGADMINPASSWORD}
 
-until nslookup ${PGHOST}; do
-  echo "waiting for ${PGHOST} to come up";
-  sleep 2;
-done
-
 until psql -U ${PGADMINUSER} -h ${PGHOST} -d postgres -l > /dev/null; do
   echo "waiting for postgres on ${PGHOST} to become ready";
   sleep 2;
